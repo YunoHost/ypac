a0227c0b40e735fab9f625b9a453e4ac33ff26ac
Laurent Peuch <cortex@worlddomination.be>
2016-07-15 22:01:54 +0200

    [enh] add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='ypac',
+      version='0.1',
+      description='YPaC is Yunohost Package Creator',
+      author='opi',
+      long_description=open("README.md", "r").read(),
+      author_email='opi@zeropi.net',
+      url='https://github.com/opi/ypac',
+      install_requires=open("requirements.txt", "r").read().split(),
+      entry_points={
+        'console_scripts': [
+            'ypac = ypac.main'
+        ],
+      },
+      license= '',
+      keywords='yunohost',
+     )
